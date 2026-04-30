610f274669de811d0ddcef10e4f95deef8e35ad1
Specular <system@specular.dev>
2026-04-30 01:20:55 +0000

    Initialize with Specular template

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,8 @@
 import { createApplication } from "@specific-dev/framework";
 import * as schema from './db/schema/schema.js';
-import * as validateClaimRoutes from './routes/validate-claim.js';
-import * as emergencyCheckRoutes from './routes/emergency-check.js';
-import * as emergencyQuestionsRoutes from './routes/emergency-questions.js';
-import * as emergencyNextQuestionRoutes from './routes/emergency-next-question.js';
-import * as reviewsRoutes from './routes/reviews.js';
-import * as drugInteractionsRoutes from './routes/drug-interactions.js';
-import * as nutritionMythsRoutes from './routes/nutrition-myths.js';
-import * as funFactsRoutes from './routes/fun-facts.js';
+
+// Import route registration functions
+// import { registerUserRoutes } from './routes/users.js';
 
 // Create application with schema for full database type support
 export const app = await createApplication(schema);
@@ -17,14 +12,7 @@ export type App = typeof app;
 
 // Register routes - add your route modules here
 // IMPORTANT: Always use registration functions to avoid circular dependency issues
-validateClaimRoutes.register(app, app.fastify);
-emergencyCheckRoutes.register(app, app.fastify);
-emergencyQuestionsRoutes.register(app, app.fastify);
-emergencyNextQuestionRoutes.register(app, app.fastify);
-reviewsRoutes.register(app, app.fastify);
-drugInteractionsRoutes.register(app, app.fastify);
-nutritionMythsRoutes.register(app, app.fastify);
-funFactsRoutes.register(app, app.fastify);
+// registerUserRoutes(app);
 
 await app.run();
 app.logger.info('Application running');
